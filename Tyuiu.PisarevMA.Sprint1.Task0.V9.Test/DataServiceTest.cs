d8cb7eb55e8a6557d09cad9c5bd937dbfa4080ff
Tyuiu.PisarevMA@outlook.com <frisssn_da@mail.ru>
2025-09-28 19:24:19 +0500

    Add project files.

---
+++ Tyuiu.PisarevMA.Sprint1.Task0.V9.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.PisarevMA.Sprint1.Task0.V9.Lib;
+namespace Tyuiu.PisarevMA.Sprint1.Task0.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(3, res);
+        }
+    }
+}
