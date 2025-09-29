ec28e37c892b1c19f7a576a45de0b6a3b23062ce
Tyuiu.PisarevMA@outlook.com <frisssn_da@mail.ru>
2025-09-29 17:44:27 +0500

    Task4

---
+++ Tyuiu.PisarevMA.Sprint1.Task4.V28.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.PisarevMA.Sprint1.Task4.V28.Lib;
+namespace Tyuiu.PisarevMA.Sprint1.Task4.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TCalcTest()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 1;
+            double res = 0.007;
+            Assert.AreEqual(res, ds.Calculate(x, y));
+        }
+    }
+}
---
+++ Tyuiu.PisarevMA.Sprint1.Task4.V28/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.PisarevMA.Sprint1.Task4.V28.Lib;
+DataService ds = new DataService();
+Console.Title = "Спринт 1 | Выполнил: Писарев М. А. | АСОиУб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
+Console.WriteLine("* Задание #4                                                              *");
+Console.WriteLine("* Вариант #28                                                             *");
+Console.WriteLine("* Выполнил: Писарев Максим Алексеевич | АСОиУб-25-1                       *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+Console.WriteLine("* вычисляет указанные расчёты и печатает результат на экране.             *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+double x, y;
+
+Console.WriteLine("Введите x: ");
+x = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine("Введите y: ");
+y = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("cos(60*pi / 2) / Exp(2 * x + y)= " + ds.Calculate(x, y));
+
+Console.ReadKey();
