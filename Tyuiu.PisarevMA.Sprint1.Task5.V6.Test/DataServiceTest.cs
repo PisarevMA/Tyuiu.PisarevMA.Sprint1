c94749c6784d0ed165af9c43716b580dce7efed9
Tyuiu.PisarevMA@outlook.com <frisssn_da@mail.ru>
2025-09-29 17:54:05 +0500

    Task5

---
+++ Tyuiu.PisarevMA.Sprint1.Task5.V6.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.PisarevMA.Sprint1.Task5.V6.Lib;
+namespace Tyuiu.PisarevMA.Sprint1.Task5.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            int k = 20;
+
+            DataService ds = new DataService();
+            double res = ds.Calculate(k);
+
+            int result = Convert.ToInt32(res);
+
+            int wait = 6;
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
---
+++ Tyuiu.PisarevMA.Sprint1.Task5.V6/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.PisarevMA.Sprint1.Task5.V6.Lib;
+DataService ds = new DataService();
+Console.Title = "Спринт 1 | Выполнил: Писарев М. А. | АСОиУб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
+Console.WriteLine("* Задание #5                                                              *");
+Console.WriteLine("* Вариант #6                                                              *");
+Console.WriteLine("* Выполнил: Писарев Максим Алексеевич | АСОиУб-25-1                       *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+Console.WriteLine("* вычисляет указанные расчёты и печатает результат на экране.             *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Введите день года: ");
+int k = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+int res = Convert.ToInt32(ds.Calculate(k));
+Console.WriteLine("День недели: " + res);
+
+Console.ReadKey();
