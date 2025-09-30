0102e237dda9c9574c750540d1d5f9f1ce34d024
Tyuiu.PisarevMA@outlook.com <frisssn_da@mail.ru>
2025-09-30 19:33:06 +0500

    Task7

---
+++ Tyuiu.PisarevMA.Sprint1.Task7.V1/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.PisarevMA.Sprint1.Task7.V1.Lib;
+DataService ds = new DataService();
+Console.Title = "Спринт 1 | Выполнил: Писарев М. А. | АСОиУб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+Console.WriteLine("* Задание #7                                                              *");
+Console.WriteLine("* Вариант #1                                                              *");
+Console.WriteLine("* Выполнил: Писарев Максим Алексеевич | АСОиУб-25-1                       *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая вычисляет математическое выражение по       *");
+Console.WriteLine("* исходным значениям данных, вводимых пользователем.                      *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine();
+Console.WriteLine("Введите значение A:");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение B:");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение C:");
+double c = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+double z = ds.Calculate(a, b, c);
+Console.WriteLine(z);
+
+Console.ReadKey();
